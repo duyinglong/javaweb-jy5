89a13c044a08b516e80dc96e63b8652f6f788407
duyinglong <52806185+duyinglong@users.noreply.github.com>
2019-08-02 20:32:18 +0800

    第二次提交

---
+++ Rlg_1/src/com/itdr/utils/JurisDictionFilter.java
@@ -0,0 +1,50 @@
+package com.itdr.utils;
+
+import com.itdr.common.ResponseCode;
+import com.itdr.pojo.Users;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(filterName = "JurisDictionFilter",value = "/manage/*")
+public class JurisDictionFilter implements Filter {
+    public void destroy () {
+    }
+
+    public void doFilter ( ServletRequest req, ServletResponse resp, FilterChain chain ) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
+        ResponseCode rs=new ResponseCode();
+        HttpServletRequest request=(HttpServletRequest)req;
+        String pathInfo=request.getPathInfo();
+        if(pathInfo.equals("/login.do")){
+            chain.doFilter(req, resp);
+            return;
+        }
+        HttpSession session=request.getSession();
+        Users user=(Users) session.getAttribute("user");
+        if(user==null){
+            rs.setStatus(3);
+            rs.setMag("请登录后操作");
+            resp.getWriter().write(rs.toString());
+            return;
+        }
+        if(user.getType()!=1){
+          rs.setStatus(3);
+          rs.setMag("没有操作权限");
+          resp.getWriter().write(rs.toString());
+          return;
+        }
+        chain.doFilter(req,resp);
+        return;
+
+    }
+
+    public void init ( FilterConfig config ) throws ServletException {
+
+    }
+
+}
